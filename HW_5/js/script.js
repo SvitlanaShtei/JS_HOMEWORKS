683d78492d96861da57c50872b93eb9f80b6d5f2
ivan <svitlana.shtei@gmail.com>
2024-02-25 03:43:59 +0100

    init

---
+++ HW_5/js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let userName;
+// За допомогою prompt запитати ім'я користувача
+userName = prompt('What is your name?');
+// За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
+alert('Hello, ' + userName +'! How are you?');
