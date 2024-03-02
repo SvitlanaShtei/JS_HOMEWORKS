037681a4db430e944828398dbb482702ebd22092
ivan <svitlana.shtei@gmail.com>
2024-03-02 05:51:34 +0100

    init

--- HW_7/js/script.js
+++ HW_7/js/script.js
@@ -1,30 +1,27 @@
 'use strict';
 
 // Task #1
-// змінні: admin та name
-let admin, name;
 
-// Присвоєння рядка "John"
+let admin;
+let name;
+
 name = "John";
-// значення змінної name до змінної admin
 admin = name;
-// виводить admin
+
 alert(admin);
 
 // Task #2
-// Змінна назви нашої планети
-const planetName = 'Earth';
 
-// Змінна для зберігання імені відвідувача сайту
+const planetName = 'Earth';
 const userName = 'Anna';
 
 // Task #3
 
-// Створення змінної та збереження в ній значення 10 (рядок) + 10 (число)
-let result = "10" + 10;
+const result = "10" + 10;
 
 // Виведення типу значення в консоль
 console.log(typeof result);
 
 // Коли рядок додається до числа то вони обєднуються в 1010
 console.log(result);
+
