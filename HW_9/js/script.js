e25dc3875995d7d48b43763aac4ec3efafb1c735
ivan <svitlana.shtei@gmail.com>
2024-03-04 04:37:24 +0100

    init

---
+++ HW_9/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const temperature = +prompt('Введіть температуру:');
+const unit = prompt('Введіть одиницю виміру (C або F):');
+
+if (!isNaN(temperature)) {
+    let result = null;
+
+    if (unit.toUpperCase() === 'C') {
+        result = `${temperature} градусів Цельсія = ${(temperature * 9 / 5) + 32} градусам Фаренгейта`;
+    } else if (unit.toUpperCase() === 'F') {
+        result = `${temperature} градусів Фаренгейта = ${(temperature - 32) * 5 / 9} градусам Цельсія`;
+    }
+    alert(result);
+} else {
+    alert('Помилка');
+}
