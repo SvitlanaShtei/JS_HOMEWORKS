5ea2f0a663ad043889bbaaa2e2f8e190122fcd0c
ivan <svitlana.shtei@gmail.com>
2024-03-13 16:37:16 +0100

    init

---
+++ HW_16/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const num1 = +prompt('Введіть перше число:');
+const num2 = +prompt('Введіть друге число:');
+const num3 = +prompt('Введіть третє число:');
+
+const result = (num1 + num2 + num3) / 3;
+
+alert('Середнє арифметичне чисел: ' + result);
+
