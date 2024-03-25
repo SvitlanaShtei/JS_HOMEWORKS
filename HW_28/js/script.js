d288e6b73288f07a246e2c0803b596f5e0fc2f20
ivan <svitlana.shtei@gmail.com>
2024-03-25 04:38:52 +0100

    init

--- HW_28/js/script.js
+++ HW_28/js/script.js
@@ -3,12 +3,12 @@
 const num1 = +prompt('Введіть число:');
 const num2 = +prompt('Введіть ступінь:');
 
-function power({base, exponent = 1}) {
+function power(base, exponent = 1) {
     if (isNaN(base) || isNaN(exponent)) {
         return 'Помилка';
     }
-    return base ** exponent;
+    return Math.pow(base, exponent);
 }
 
-const result = power({base: num1, exponent: num2});
+const result = power(num1, num2);
 alert(result);
